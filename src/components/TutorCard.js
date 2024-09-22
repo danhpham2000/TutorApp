f1eff904af617c95d27111999f7c05376a256e88
Ethan H. Nguyen <ethanhducnguyen@gmail.com>
2024-09-22 03:52:08 -0500

    merged home with fundtutors

--- src/components/TutorCard.js
+++ src/components/TutorCard.js
@@ -4,38 +4,14 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import CardGroup from '../../node_modules/react-bootstrap/esm/CardGroup';
 
-function TutorCard() {
+const TutorCard = ({ titles }) => {
   return (
-    // <CardGroup style={{ margin: '0 10em', padding: "10px"}}>
-    //   <Card>
-    //     <Card.Body>
-    //       <Card.Title>Card title</Card.Title>
-    //     </Card.Body>
-    //   </Card>
-    //   <Card>
-    //     <Card.Body>
-    //       <Card.Title>Card title</Card.Title>
-    //     </Card.Body>
-    //   </Card><Card>
-    //     <Card.Body>
-    //       <Card.Title>Card title</Card.Title>
-    //     </Card.Body>
-    //   </Card>
-    // </CardGroup>
-
-    // We used Grid Cards from group
     <Row xs={1} md={3} className="g-4" style={{margin: "0 5em"}}>
-      {Array.from({ length: 3 }).map((_, idx) => (
+      {titles.map((title, idx) => (
         <Col key={idx}>
           <Card style={{width: "350px"}}>
-            <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
+              <Card.Title>{title}</Card.Title>
             </Card.Body>
           </Card>
         </Col>
