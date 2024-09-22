f60166ba0f806453e1cbbe2b45fee4c07b04517a
Haris <malikharis584@gmail.com>
2024-09-22 03:28:50 -0500

    updated cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,15 @@ import LandingPageSection from "./LandingPageSection"
 import TutorCard from './TutorCard'
 
 export default function Home() {
+  const cardTitles1 = ["Math Tutor", "Computer Science Tutor", "Physics Tutor"];
+  const cardTitles2 = ["Chemistry Tutor", "English Tutor", "History Tutor"];
+  const cardTitles3 = ["Accounting Tutor", "Engineering Tutor", "Biology Tutor"];
   return (
     <div>
         <LandingPageSection/>
-        <TutorCard/>
-        <TutorCard/>
-        <TutorCard/>
+        <TutorCard titles={cardTitles1}/>
+        <TutorCard titles={cardTitles2}/>
+        <TutorCard titles={cardTitles3}/>
         <br/>
         
     </div>
